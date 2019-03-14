aabc658bdbb2faf429f4b89b7e85b69b9bda1122
Renzo Ezagui <renzo.ezagui@gmail.com>
2019-03-13 22:38:05 -0300

    init

---
+++ database/seeds/DatesTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Dates:truncate();
+        
+    }
+}

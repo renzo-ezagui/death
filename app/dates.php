5157d3378fbc446f3e8dee7bb165bf5490b07238
Renzo Ezagui <renzo.ezagui@gmail.com>
2019-03-13 23:02:16 -0300

    init

--- app/dates.php
+++ app/dates.php
@@ -4,6 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\dates
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $date
+ * @property int $user
+ * @property string $message
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereMessage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\dates whereUser($value)
+ */
 class dates extends Model
 {
     //
--- database/seeds/DatesTableSeeder.php
+++ database/seeds/DatesTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\dates;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatesTableSeeder extends Seeder
@@ -12,7 +14,16 @@ class DatesTableSeeder extends Seeder
     public function run()
     {
         //
-        Dates:truncate();
+        dates::truncate();
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            dates::create([
+                'user' => $faker->numberBetween(0,20),
+                'date' => $faker->date('Y-m-d','now'),
+                'message' => $faker->sentence,
+            ]);
+        }
         
     }
 }
